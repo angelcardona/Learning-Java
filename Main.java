390f27e772af0faac11cb84a0eeec6983d2b118e
angelcardona <miguel.acardona98@gmail.com>
2025-04-22 11:01:49 -0500

    Starting with java-strings

--- Main.java
+++ Main.java
@@ -8,13 +8,25 @@ public class Main {
         char curso='A';
         //Strings
         String text="Hola mundo";
+        String texto=text.replace("Hola", "chao");
+        String bad="  pork happy  ";
+        //Print by console
         System.out.println(edad);
         System.out.println(numeroGrande);
         System.out.println(age);
         System.out.println(isActive);
         System.out.println(curso);
         System.out.println(text);
+        System.out.println(texto);
+        //Strings methods
         System.out.println(text.length());
+        System.out.println(texto.endsWith("mundo"));
+        System.out.println(texto.startsWith("o"));
+        System.out.println(texto.contains("und"));
+        System.out.println(texto.indexOf("mund"));
+        System.out.println(texto.toUpperCase());
+        System.out.println(texto.toLowerCase());
+        System.out.println(bad.trim());
     }
 }
 
