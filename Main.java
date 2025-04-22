0153ddefc5d8709c6967b1ac5300caf8fdbae238
angelcardona <miguel.acardona98@gmail.com>
2025-04-22 10:40:45 -0500

    Starting with java

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        //Datos primitivos
+        byte edad=18;
+        int numeroGrande=3546;
+        float age=15.5f;
+        boolean isActive=true;
+        char curso='A';
+        //Strings
+        String text="Hola mundo";
+        System.out.println(edad);
+        System.out.println(numeroGrande);
+        System.out.println(age);
+        System.out.println(isActive);
+        System.out.println(curso);
+        System.out.println(text);
+        System.out.println(text.length());
+    }
+}
+
+
