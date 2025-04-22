920f4f75e3ef836c3a672b263aa18ef6675c182d
angelcardona <miguel.acardona98@gmail.com>
2025-04-22 11:28:29 -0500

    Created Escapecharacters

---
+++ EscapeCharacters.java
@@ -0,0 +1,20 @@
+public class EscapeCharacters {
+    public static void main(String[] args) {
+        String text = "- Hello world";
+        System.out.println(text);
+        // Using ""
+        String text1="- Hello \"World\"";
+        System.out.println(text1);
+        //Usinng \n
+        String text2= "- Hello \n  World";
+        System.out.println(text2);
+        //Using \t
+        String  text3="- Hello \tWorld";
+        System.out.println(text3);
+
+
+
+
+    }
+    
+}
