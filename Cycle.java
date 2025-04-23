09f9e09c2a55ccad3c4d8eae00c788e0784bbeba
angelcardona <miguel.acardona98@gmail.com>
2025-04-23 10:43:48 -0500

    loops

---
+++ Cycle.java
@@ -0,0 +1,19 @@
+public class Cycle {
+    public static void main(String[] args) {
+        String[] quoutes={"love","peace","calm","growth","integrity"};
+        System.out.println(quoutes.length);
+        
+        for (int i=0;i< quoutes.length;i++){
+            System.out.println(quoutes[i]);
+        }
+
+        int i = 0;
+        while (i < quoutes.length ) {
+            System.out.println(quoutes[i]);
+            i++;
+
+            
+        }
+    }
+    
+}                                                                                                                  
