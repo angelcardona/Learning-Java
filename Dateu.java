0c7b6f87913a4ee748d4a2538dcfab29d5145053
angelcardona <miguel.acardona98@gmail.com>
2025-04-24 09:38:38 -0500

    Working wit dates

---
+++ Dateu.java
@@ -0,0 +1,31 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+
+public class Dateu {
+    public static void main(String[] args) {
+        
+        Date date=new Date();
+        System.out.println("Current date" + date);
+
+        //Giving format
+        SimpleDateFormat format= new SimpleDateFormat("yyyy/MM/dd");
+
+        //Foramting the date
+        String dateformat=format.format(date);
+        System.out.println("Currente date: " + dateformat);
+
+        //Using Date time formater
+        //Create a DateTimeFormater with the desire pattern
+       
+        DateTimeFormatter formating=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        //Current Date
+        LocalDate date2=LocalDate.now();
+
+        String currentDate=date2.format(formating);
+        System.out.println(currentDate);
+
+    }
+}
